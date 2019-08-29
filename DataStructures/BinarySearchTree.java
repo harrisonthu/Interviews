e5417d9abedf4546a5e0080762e4cd0cad782dd6
Han Thu <hmt4224@g.rit.edu>
2019-08-28 23:24:35 -0400

    Create BinarySearchTree.java

---
+++ DataStructures/BinarySearchTree.java
@@ -0,0 +1,31 @@
+public class BinarySearchTree {
+
+    // Create nested class for Node class
+    public class Node{
+        int key;
+        Node left;
+        Node right;
+    }
+
+    // Root of the Node
+    Node root;
+
+    //Constructor ==> contain Node ==> NULL
+    BinarySearchTree(){
+        this.root = null;
+    }
+
+    //Insertion
+    // value = new value that I want to insert
+    void Insertion(int value){
+
+    }
+
+
+
+
+
+
+
+    //End of BinarySearchTree Class
+}
