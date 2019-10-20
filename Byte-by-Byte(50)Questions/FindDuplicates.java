16cf27caed197d695e79bb215ea2c7a9871725c7
Han Thu <hmt4224@g.rit.edu>
2019-10-20 14:04:25 -0400

    Create FindDuplicates.java

---
+++ Byte-by-Byte(50)Questions/FindDuplicates.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+
+// This question is from Page 8 from 50 Interview Questions (Byte by Byte)
+// Find Duplicates 
+// Question(Q4) :  Given an array of integers where each value 1 <=x<=len(array)
+// write a function that ﬁnds all the duplicates in the array
+// Solution:  https://www.byte-by-byte.com/findduplicates/?utm_source=optin_carrot&utm_medium=pdf&utm_campaign=50questions
+//
+public class FindDuplicates {
+
+
+    public static HashSet FindDuplicates(int[] arr) {
+        // If the input array is empty, return
+        if (arr.length == 0) {
+            return new HashSet();
+        }
+
+        HashSet<Integer> myhs = new HashSet<Integer>();
+        HashSet<Integer> dupeslist = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            // if current value in arry contain in dupeslist<Hashset>
+            if (myhs.contains(arr[i])) {
+                dupeslist.add(arr[i]);
+                continue;
+            }
+            // Not included in dupeslist, so add it to dupeslist
+            else {
+                myhs.add(arr[i]);
+            }
+        }
+        return dupeslist;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2 , 1, 2, 1,7,8,8,8,8,8};
+        System.out.println(FindDuplicates(arr));
+
+    }
+}
