db0bcb52ee1ec029fd5bc880022ce9556e1bb194
Han Thu <hmt4224@g.rit.edu>
2019-12-22 21:10:25 -0500

    Update median.java

--- Byte-by-Byte(50)Questions/median.java
+++ Byte-by-Byte(50)Questions/median.java
@@ -3,37 +3,67 @@ import java.util.Arrays;
 import java.util.List;
 
 // This question is from Page 1 from 50 Interview Questions (Byte by Byte)
-// Median of Arrays 
-// Question(Q1) : Find the median of two sorted arrays. 
+// Median of Arrays
+// Question(Q1) : Find the median of two sorted arrays.
 // Solution:  https://www.byte-by-byte.com/median/ 
 //
+
 public class median {
 
-    public median() {
-
-
-    }
-
-    public static double findmedian(int[] inputarray) {
+    // Find the median (in a single array)
+    public static double FindMedian(int[] inputarray) {
         double result = 0.0;
         if (inputarray.length >= 1) {
             int n = inputarray.length;
             int mid = n / 2;
-            if (n % 2 == 0) {  // even size array
-//                if(inputarray.length>=2){  // Array size is more than 2
-//
-//                }
-
-                //result = (inputarray[mid-1]+ inputarray[mid])/2.0;
+            if (n % 2 == 0) {  // even size array n = 2, 4, 6, 8
                 return (inputarray[mid - 1] + inputarray[mid]) / 2.0;
             } else {   // odd size array
-
-                //result = (inputarray[mid]);
                 return inputarray[mid];
             }
-
         }
         return result;
+    }
+
+    // Find the median (in two arrays)
+    public static double FindMedian2(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int o = n + m;
+        int[] resultarr = new int[o];
+
+
+        // Using Mergesort
+        int i = 0; // index for arr1
+        int j = 0; // index for arr2
+        int k = 0; // index for resultarr
+
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                resultarr[k] = arr1[i];
+                i++;
+            } else { // arr2 [j] > arr1[i]
+                resultarr[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n) {
+            resultarr[k] = arr1[i];
+            i++;
+        }
+
+        while (j < m) {
+            resultarr[k] = arr2[j];
+            j++;
+        }
+        List<Integer> printout = new ArrayList<Integer>();
+        for (int b : resultarr) {
+            printout.add(b);
+        }
+        System.out.println("printout: " + printout);
+
+        return FindMedian(resultarr);
     }
 
 
@@ -41,44 +71,8 @@ public class median {
         int[] arr1 = {1, 3, 5};
         int[] arr2 = {2, 4, 6};
 
-        int n = arr1.length;
-        int m = arr2.length;
-
-        // create the array to store the final sorted array
-        int finalsize = (int) n + m;
-        int[] finalsortedarray = new int[finalsize];
-
-        //using Mergesort idea to sort the two sorted arrays.
-        int i = 0; // indexes for arr1
-        int j = 0; // indexes for arr2
-        int k = 0; // indexes for finalsortedarray
-
-        // compare values from two arrays
-        while (i < n && j < m) {
-
-            if (arr1[i] < arr2[j]) {
-                finalsortedarray[k] = arr1[i];
-                i++;
-            } else {
-                finalsortedarray[k] = arr2[j];
-                j++;
-            }
-            k++;
-        }
-        // if the two input arrays are not same size
-        // add the rest of the values from larger size array
-        while (i < n) { // if arr1 size is larger than arr2
-            finalsortedarray[k] = arr1[i];
-            k++;
-            i++;
-        }
-        while (j < m) { // if arr2 size is larger than arr1
-            finalsortedarray[k] = arr2[j];
-            k++;
-            j++;
-        }
-        System.out.println(Arrays.toString(finalsortedarray));
-        System.out.println(findmedian(finalsortedarray));
+        FindMedian2(arr1, arr2);
+        System.out.println(FindMedian2(arr1, arr2));
     }
 
 }
